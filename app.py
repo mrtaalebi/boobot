72c8cf010919ff49c2803d5363c90fe7eccb5a70
mrtaalebi <the.doors.are.locked@gmail.com>
2020-06-08 02:12:17 +0430

    Add class

--- app.py
+++ app.py
@@ -1,32 +1,34 @@
 import os
-import logging
 
 from telegram.ext import Updater, CommandHandler
 
 
-updater = Updater(token=os.environ.get('BOT_TOKEN'), use_context=True)
-dispatcher = updater.dispatcher
+class Boobot:
 
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO
-)
+    def __init__(self, bot_token):
+        self.updater = Updater(bot_token, use_context=True)
+        self.dispatcher = self.updater.dispatcher
 
 
-def start(update, context):
-    keyboard = [
-        InlineKeyboardButton('get telegram proxy'),
-        InlineKeyboardButton('get openconnect data'),
-        InlineKeyboardButton('get openvpn data'),
-    ]
-    
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    context.bot.send_message(chat_id=update.message.chat_id,
-        reply_markup=reply_markup,
-    )
+    def start(self, update, context):
+        keyboard = [
+            InlineKeyboardButton('get telegram proxy'),
+            InlineKeyboardButton('get openconnect data'),
+            InlineKeyboardButton('get openvpn data'),
+        ]
+        
+        reply_markup = InlineKeyboardMarkup(keyboard)
+        context.bot.send_message(
+            chat_id=update.message.chat_id,
+            reply_markup=reply_markup,
+        )
 
 
-start_handler = CommandHandler('start', start)
-dispatcher.add_handler(start_handler)
+    def add_handlers(self):
+        start_handler = CommandHandler('start', start)
+        self.dispatcher.add_handler(start_handler)
 
-updater.start_polling()
+
+    def run(self):
+        self.add_handlers()
+        self.updater.start_polling()
