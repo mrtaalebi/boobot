b70c3ebb114515c59efc5e73eb2b7a0ec653ae15
mrtaalebi <the.doors.are.locked@gmail.com>
2020-06-08 06:08:41 +0430

    Add a heavy load of untested shit

---
+++ src/app.py
@@ -0,0 +1,185 @@
+import os
+import logging
+import json
+
+
+from telegram.ext import Updater, CommandHandler
+from telegram import InlineKeyboardButton, ReplyKeyboardMarkup
+
+from src.db import DB
+
+
+class Boobot:
+
+    def __init__(self, bot_token, engine_uri, log_level='INFO'):
+        self.updater = Updater(bot_token, use_context=True)
+        self.dispatcher = self.updater.dispatcher
+
+        self.db = DB(engine_uri)
+
+        logging.basicConfig(
+            format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+            level={
+                'INFO': logging.INFO,
+                'DEBUG': logging.DEBUG,
+                'ERROR': logging.ERROR,
+                }[log_level]
+        )
+
+ 
+    def build_callback(data):
+        return_value = json.dumps(data)
+        if len(return_value) > 64:
+            raise Exception("Callback data is larger tan 64 bytes")
+        return return_value
+
+
+    def send_keyboard(self, update, keyboard, text):
+        reply_keyboard = ReplyKeyboardMarkup(keyboard)
+        update.message.reply_text(
+            text=text,
+            reply_markup=reply_keyboard
+        )
+
+
+    def start(self, update, context):
+        user = self.db.get_or_create_user(update['from'])
+        keyboard = [
+            InlineKeyboardButton(option['text'])
+            for option in
+                [
+                    {
+                        'text': msg.get('openconnect', user),
+                        'data': build_callback(
+                            {
+                                'd1': 'oc',
+                                'd2': 'm1'
+                            })
+                    },
+                ]    
+            ]
+        ]
+        self.send_keyboard(update, keyboard, 'main menu')
+
+
+    def openconnect(self, update, context):
+        keyboard = [
+            InlineKeyboardButton('show openconnect data'),
+            InlineKeyboardButton('add openconnect account')
+        ]
+        self.send_keyboard(update, keyboard, 'openconnect')
+
+
+    def openconnect_show_data(self, update, context):
+        oc_config = self.db.get_oc_config(update['from']['id'])
+        if oc_config == None:
+            keyboard = [
+                InlineKeyboardButton('add openconnect account'),
+                InlineKeyboardButton('main menu'),
+            ]
+            self.send_keyboard(update, keyboard, 'nothing here!')
+        else:
+            keyboard = [
+                InlineKeyboardButton('main menu'),
+            ]
+            self.send_keyboard(update, keyboard, str(oc_config))
+
+
+    def openconnect_add_data(self, update, context):
+        user = self.db.get_or_create_user(update['from'])
+        keyboard = [
+            InlineKeyboardButton('openconnect'),
+            InlineKeyboardButton('main menu'),
+        ]
+        msg = 'enter a username for openconnect:'
+        self.send_keyboard(update, keyboard, msg)
+        return 'openconnect_username'
+
+
+    def openconnect_add_data_username(self, update, context):
+        user = self.db.get_or_create_user(update['from'])
+        text = update.message.text
+        keyboard = [
+            InlineKeyboardButton('openconnect'),
+            InlineKeyboardButton('main menu'),
+        ]
+        if re.match(text, '(a-zA-Z)(a-zA-Z0-9)+') and \
+                len(db.query(OC_Config, username == text)) == 0:        
+            s = self.db.session()
+            user = s.query(User).filter(User.id == user.id).first()
+            user.oc_username = text
+            s.commit()
+            msg = 'now choose a strong password:'
+            self.send_keyboard(update, keyboard, msg)
+            return 'openconnect_password'
+        else:
+            msg = (
+                    'username must start with a-zA-Z\n'
+                    'contain only a-zA-Z0-9\n'
+                    'and be atleast 3 characters\n'
+            )
+            self.send_keyboard(update, keyboard, msg)
+            return 'openconnect_username'
+
+
+    def openconnect_add_data_password(self, update, context):
+        user = self.db.get_or_create_user(update['from'])
+        text = update.message.text
+        keyboard = [
+            InlineKeyboardButton('openconnect'),
+            InlineKeyboardButton('main menu'),
+        ]
+        if len(text) >= 8:
+            s = self.db.session()
+            user = s.query(User).filter(User.id == user.id).first()
+            user.oc_password = text
+            s.commit()
+            
+            subprocess.run(
+                [
+                    'ocpasswd',
+                    '-c',
+                    '/etc/ocserv/pass.wd',
+                    user.oc_username
+                ],
+                input=f'{user.oc_password}\n{user.oc_password}\n',
+            )
+
+            msg = 'nice! your openconnect account will be ready in a second.'
+            self.send_keyboard(update, keyboard, msg)
+            return -1
+        else:
+            msg = 'password must be >=8 characters'
+            self.send_keyboard(update, keyboard, msg)
+            return 'openconnect_password'
+                    
+
+    def add_handlers(self):
+        start_handler = CommandHandler('start', self.start)
+        self.dispatcher.add_handler(start_handler)
+
+        mainmenu_handler = RegexHandler('^main menu$', self.main_start)
+        self.dispatcher.add_handler(mainmenu_handler)
+
+        openconnect_handler = RegexHandler('^openconnect$', self.openconnect)
+        self.dispatcher.add_handler(openconnect_handler)
+
+        openconnect_show_data_handler = \
+            RegexHandler('^show openconnect data$', self.openconnect_show_data)
+        self.dispatcher.add_handler(openconnect_show_data_handler)
+
+        openconnect_add_data_handler = ConversationHandler(
+            entry_points=[RegexHandler(
+                '^add openconnect account$',
+                self.openconnect_add_data)],
+            states={
+                'openconnect_username': [RegexHandler('^.*|/cancel$', self.openconnect_add_data_username)],
+                'openconnect_password': [RegexHandler('^.*|/cancel$', self.openconnect_add_data_password)],
+            },
+            fallbacks=[CommandHandler('cancel', self.cancel)],
+        )
+
+
+    def run(self):
+        self.add_handlers()
+        self.updater.start_polling()
---
+++ src/db.py
@@ -0,0 +1,78 @@
+from sqlalchemy import Column, Integer, String, create_engine 
+from sqlalchemy.ext.declarative import declrative_base
+from sqlalchemy.orm import sessionmaker
+
+
+Base = declarative_base()
+
+
+class User(Base):
+    
+    __tablename__ = 'user'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100))
+    lang = Column(String(5))
+    oc_username = Column(String(20))
+    oc_password = Column(String(64))
+
+
+    def __init__(self, id, name, lang, oc_username=None, oc_password=None):
+        self.id = id
+        self.name = name
+        self.lang = lang
+        self.oc_username = oc_username
+        self.oc_password = oc_password
+
+
+class DB:
+
+    def __init__(self, uri):
+        self.engine = create_engine(uri)
+
+    
+    def session(self):
+        s = sessionmaker()
+        s.configure(bind=self.engine)
+        return s()
+
+    
+    def add(self, entry):
+        s = self.session()
+        s.add(entry)
+        s.commit()
+
+    
+    def query(self, class_, *filter_=[]):
+        s = self.session()
+        q = s.query(class_)
+        for f in filter_:
+            q = q.filter(f)
+        return q
+
+    
+    def get_or_create_user(self, from_):
+        user_id = from_['id']
+        q = self.query(User, User.id == user_id)
+        if len(q) == 0:
+            user = User(
+                    id=user_id,
+                    name=from_['first_name'],
+                    lang=from_['language-code'],
+                    oc_username='',
+                    oc_password='',
+                )
+            self.add(user)
+            options = [
+
+            ]
+            return user
+        else:
+            return q.first()
+
+
+    def get_oc_config(self, user_id):
+        q = self.query(OC_Config, OC_Config.user_id = user_id)
+        if len(q) == 0:
+            return None
+        elif len(q) == 1:
+            return q.first()
